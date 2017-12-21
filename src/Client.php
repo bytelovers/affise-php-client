69a18ebd3db471f43078538399e46373aafe854b
Alfredo De la calle <alfredodelacalle@MacBook-Pro-de-Alfredo.local>
2017-12-21 12:29:41 +0100

    Initial commit

---
+++ src/Client.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace Bytelovers\Affise;
+
+    class Client {
+        private $apiUrl = 'http://';
+
+        public function __construct() {
+            return $this->apiUrl;
+        }
+    }
