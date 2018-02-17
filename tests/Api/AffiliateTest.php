59d5ab73a710aba13089981f812988ddcdcf2ce6
Alfredo <alfredo.delacalle@gmail.com>
2018-02-17 09:07:52 +0100

    Tests

---
+++ tests/Api/AffiliateTest.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace Bytelovers\Affise\Test;
+
+    use PHPUnit\Framework\TestCase;
+    use Bytelovers\Affise\Api\Affiliate\Partner;
+
+    class AffiliateTest extends TestCase {
+
+        public function getOfferListTest() {}
+    }
